b3daef7c58292ea4d7688e7d09d6a537b7f70898
Bruce Chen <BruceChen@kingstono365.onmicrosoft.com>
2016-11-10 14:09:11 +0800

    Remove unuse using.

--- SystemDataSQLiteConsole/SystemDataSQLiteConsole/Program.cs
+++ SystemDataSQLiteConsole/SystemDataSQLiteConsole/Program.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Data.Common;
-using System.Data.SQLite;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SystemDataSQLiteConsole
 {
